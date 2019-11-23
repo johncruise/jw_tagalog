e3c8ca68656fdf58a40448f3535cb715c102aa8a
John Cruz <johncruise+github.com@gmail.com>
2019-11-23 00:20:08 -0800

    Fix  new URL

--- getclmstudynum.py
+++ getclmstudynum.py
@@ -18,7 +18,7 @@ URL_HEADER = "https://www.jw.org"
 # months = ["enero", "pebrero", "marso", "abril", "mayo", "hunyo", "hulyo", "agosto", "setyembre",
 # 	"oktubre", "nobyembre", "disyembre"]
 months = ["january", "february", "march", "april", "may", "june", "july", "august", "september",
-	"octuber", "november", "descember"]
+	"october", "november", "december"]
 
 
 def main(month, year):
@@ -32,8 +32,9 @@ def main(month, year):
 	- `None`
 	"""
 	try:
-		pageurl = 'https://www.jw.org/en/publications/jw-meeting-workbook/{}-{}-mwb/'.format(
+		pageurl = 'https://www.jw.org/en/library/jw-meeting-workbook/{}-{}-mwb/'.format(
 			months[month - 1], year)
+		# print('Opening workbook for {}/{}...'.format(month, year))
 		site = urllib2.urlopen(pageurl)
 		data = site.read()
 	except urllib2.URLError as err:
@@ -43,25 +44,33 @@ def main(month, year):
 
 	# NOTE: Look for the following pattern
 	# https://www.jw.org/en/publications/jw-meeting-workbook/june-2019-mwb/meeting-schedule-june3-9/
-	res = re.findall(r'/en/publications/jw-meeting-workbook/.+?/meeting-schedule-.+?/',
+	# https://www.jw.org/en/library/jw-meeting-workbook/december-2019-mwb/meeting-schedule-december2-8/
+	# https://www.jw.org/en/library/jw-meeting-workbook/january-2020-mwb/Our-Christian-Life-and-Ministry-Schedule-for-January-6-12-2020/
+	res = None
+	res = re.findall(
+		r'/en/library/jw-meeting-workbook/.+?/(meeting-schedule|Our-Christian-Life-and-Ministry-Schedule-for)-(.+?)/',
 		data.decode('utf-8'))
 	urls = []
 	for each in res:
 		if each in urls:
 			continue
 		urls.append(each)
-	for each in urls:
-		tmp = each[1].decode("latin-1")
-		datedata = re.match(r'.+meeting-schedule-(([a-z]+)([0-9]+-[0-9]+))/', each)
+	# print('urls = {!r}'.format(urls))
+	for prefix, each in urls:
+		# tmp = each[1].decode()
+		datedata = re.match(r'([A-Za-z]+)-?([0-9]+-[0-9]+)(-[0-9]{4})?', each)
 		if datedata is not None:
-			datestr = '{} {}'.format(datedata.group(2).capitalize()[:3], datedata.group(3))
+			datestr = '{} {}'.format(datedata.group(1).capitalize()[:3], datedata.group(2))
 		else:
-			datedata = re.match(r'.+meeting-schedule-(([a-z]+)([0-9]+)-([a-z]+)([0-9]+))/', each)
+			datedata = re.match(r'([A-Za-z]+)-?([0-9]+)-([A-Za-z]+)-?([0-9]+)(-[0-9]{4})?', each)
 			datestr = '{} {}-{} {}'.format(
-				datedata.group(2).capitalize()[:3], datedata.group(3),
-				datedata.group(4).capitalize()[:3], datedata.group(5))
-		studies = grabweekstudies(URL_HEADER + each)
-		print("- {}: {!r}".format(datestr, studies))
+				datedata.group(1).capitalize()[:3], datedata.group(2),
+				datedata.group(3).capitalize()[:3], datedata.group(4))
+		# print('Grabbing week studies...')
+		studies, parts = grabweekstudies('{}{}-{}'.format(pageurl, prefix, each))
+		print("- {}: {}".format(
+			datestr,
+			', '.join(['{} #{}'.format(part, study) for study, part in  zip(studies, parts)])))
 
 
 def grabweekstudies(url):
@@ -79,14 +88,22 @@ def grabweekstudies(url):
 
 	# get the HTML-stripped data
 	tmp = site.read().decode('utf-8')
-	study = re.findall(r"<em>th.*?</em>study.*?\d+</a>", tmp)
+	haspart = True
+	study = re.findall(r"<strong>.+</strong>.+<em>th.*?</em>study.*?\d+</a>", tmp)
+	if study is None:
+		haspart = False
+		study = re.findall(r"<em>th.*?</em>study.*?\d+</a>", tmp)
 
 	# get the data enclosed in the first and last song
 	studies = []
+	parts = []
 	for each in study:
-		res = re.match('.+study.+?(\d+)</a>', each)
+		res = re.match(r'.+study.+?(\d+)</a>', each)
 		studies.append(int(res.group(1)))
-	return studies
+		if haspart:
+			res = re.match(r'<strong>(.+?):?\s*</strong>', each)
+			parts.append(res.group(1))
+	return studies, parts
 
 
 if __name__ == "__main__":
